9d2f8b467efada09bac6bea3079d9516aa3a480d
Vicente Lozano <vicentelozano@gmail.com>
2018-10-26 12:02:06 +0200

    06.09 Add an Edit preference for the size

--- Lesson06-Visualizer-Preferences/T06.09-Exercise-EditTextPreference/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.09-Exercise-EditTextPreference/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -59,7 +59,14 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
                 getResources().getBoolean(R.bool.pref_show_mid_range_default)));
         mVisualizerView.setShowTreble(sharedPreferences.getBoolean(getString(R.string.pref_show_treble_key),
                 getResources().getBoolean(R.bool.pref_show_treble_default)));
-        mVisualizerView.setMinSizeScale(1);
+        mVisualizerView.setMinSizeScale(
+                Float.parseFloat(
+                    sharedPreferences.getString(
+                            getString(R.string.pref_size_key),
+                            getString(R.string.pref_size_default)
+                    )
+                )
+        );
         loadColorFromPreferences(sharedPreferences);
         // Register the listener
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
@@ -82,6 +89,13 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
             mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_treble_default)));
         } else if (key.equals(getString(R.string.pref_color_key))) {
             loadColorFromPreferences(sharedPreferences);
+        } else if (key.equals(getString(R.string.pref_size_key))) {
+            mVisualizerView.setMinSizeScale(Float.parseFloat(
+                    sharedPreferences.getString(
+                            key,
+                            getString(R.string.pref_size_default)
+                    )
+            ));
         }
     }
 
